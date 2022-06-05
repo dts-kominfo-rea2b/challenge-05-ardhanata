0fcad47b72e28de6480928779fc2b10e4b657854
ardhanata <g.d.ardhanata@gmail.com>
2022-06-05 19:37:37 +0700

    first edit

--- index.js
+++ index.js
@@ -6,25 +6,38 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (dataName, func) => {
+    let result = func(dataName);
+    let hasil = [];
+    for (let i = 0; i < result.length; i++) {
+        hasil.push(`${i+1}. ${result[i]}`);
+    }
+    return hasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+    let result = data.sort();
+    return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+    let result = data.reverse();
+    return result;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+    console.log(sorter?.(names, sortAscending)?.join("\n"));
+    console.log(sorter?.(names, sortDescending)?.join("\n"));
 })();
 
 module.exports = {
-  sorter,
-  sortAscending,
-  sortDescending,
-  names,
+    sorter,
+    sortAscending,
+    sortDescending,
+    names,
 };
